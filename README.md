c4fac96c25444655014c420e9259944d27a501c0
lottiely <saialmario1@gmail.com>
2018-03-29 10:02:38 -0700

    Recent Updates.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 A website about me and what I do.
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app). 
+
+header-font: I intended to credit the font, but I don't remember where I found this font. I just used it in photoshop, but lost it when I switched computers. I'm currently hunting down where I found it, but if I can't find it, it is likely my header will change.
--- src/components/about.js
+++ src/components/about.js
@@ -4,11 +4,18 @@ class About extends Component {
   render() {
     return (
       <div className="content-wrapper">
+      <img src={require('../assets/images/about-me-image.JPG')} className="about-me-image" alt="About Me"/>
 		<div className="about-me-content">
-			<img src={require('../assets/images/about-me-image.JPG')} className="about-me-image" alt="About Me"/>
-			<p>Lorem ipsum dolor sit amet, ei justo partem vivendum eum, ad mel utinam lucilius adolescens. No solet facete perfecto ius, tibique percipitur ei eos. Cu mel nullam suscipiantur, at est link vituperata. Sed ne eius omnis suscipit, vix ex alia assueverit vituperatoribus. At prima maiorum urbanitas pro, modus vocibus noluisse his et.
-
-			Lorem ipsum dolor sit amet, ei justo partem vivendum eum, ad mel utinam lucilius adolescens. No solet facete perfecto ius, tibique percipitur ei eos. Cu mel nullam suscipiantur, at est link vituperata. Sed ne eius omnis suscipit, vix ex alia assueverit vituperatoribus. At prima maiorum urbanitas pro, modus vocibus noluisse his et.</p>
+			<p>I love food and trying new things!
+      <br />
+      <br />
+      I am a small town girl trying to find a place in the big city. 
+      I am currently in the Bay Area on the hunt for my new favorite boba place. 
+      I recently graduated from the University of Alaska Anchorage with a Bachelor's of Science in Computer Systems Engineering, and I am currently exploring opportunities to enter the tech industry. 
+      Topics where I have taken particular interest are front-end engineering and machine learning!
+      <br />
+      <br />
+      But when I'm not coding away, I am a baker, figure skater, artist, fiction writer, casual gamer, and fashion enthusiast.</p>
 		</div>
       </div>
     );
